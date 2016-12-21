80e3cbbe86232f6b5f25cd0cc4dc9b04c16c20b4
BuildTools <unconfigured@null.spigotmc.org>
2016-12-21 19:47:13 +0800

    Fix

---
+++ Exercise6/CodeIgniter/application/views/links.php
@@ -0,0 +1,11 @@
+<table width="1500" align="center" cellpadding="0" celspacing="0">
+<tr>
+<td>
+<td align="center">
+<font face="Eras Medium ITC" size="4" color="#ffffff">
+<body a link="black" vlink="black">
+<a href="<?php echo site_url('index');?>"><b>HOME</b></a>  ||  <a href="<?php echo site_url('HOBBIES');?>"><b>HOBBIES</b></a>  ||  <a href="<?php echo site_url('GALLERY');?>"><b>GALLERY</b></a>  ||  <a href="<?php echo site_url('FRIENDS');?>"><b>FRIENDS</b></a> || <a href = "<?php echo site_url('Contact');?>"><b>Contact Me</b></a>
+</font>
+</td>
+</tr>
+</table>
