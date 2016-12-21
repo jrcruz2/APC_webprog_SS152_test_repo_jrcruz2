acc65d72918123d9430ee5df2d3a78e1465dccb1
BuildTools <unconfigured@null.spigotmc.org>
2016-12-21 20:23:03 +0800

    DONE!

--- Exercise6/CodeIgniter/application/views/links.php
+++ Exercise6/CodeIgniter/application/views/links.php
@@ -4,7 +4,7 @@
 <td align="center">
 <font face="Eras Medium ITC" size="4" color="#ffffff">
 <body a link="black" vlink="black">
-<a href="<?php echo site_url('Home');?>"><b>HOME</b></a>  ||  <a href="<?php echo site_url('Favorites');?>"><b>HOBBIES</b></a>  ||  <a href="<?php echo site_url('GALLERY');?>"><b>GALLERY</b></a>  ||  <a href="<?php echo site_url('FRIENDS');?>"><b>FRIENDS</b></a> || <a href = "<?php echo site_url('Contact');?>"><b>Contact Me</b></a>
+<a href="<?php echo site_url('Home');?>"><b>HOME</b></a>  ||  <a href="<?php echo site_url('Favorites');?>"><b>HOBBIES</b></a>  ||  <a href="<?php echo site_url('GALLERY');?>"><b>GALLERY</b></a>  ||  <a href="<?php echo site_url('FRIENDS');?>"><b>FRIENDS</b></a> || <a href = "<?php echo site_url('users2');?>"><b>Contact Me</b></a>
 </font>
 </td>
 </tr>
