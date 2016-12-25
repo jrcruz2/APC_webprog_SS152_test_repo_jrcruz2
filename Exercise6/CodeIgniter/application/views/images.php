b7d6091f37cd87945b9aecbe1c57fc1841ac1e89
BuildTools <unconfigured@null.spigotmc.org>
2016-12-25 12:17:23 +0800

    fix

--- Exercise6/CodeIgniter/application/views/images.php
+++
@@ -1,7 +0,0 @@
-<img align="center" src="assets/img/fb.png" width="60" height="60">
-<img align="center" src="assets/img/9gag.png" width="60" height="60">
-<img align="center" src="assets/img/insta.png" width="60" height="60">
-<img align="center" src="assets/img/qwe.jpg" width="900" height="75">
-<img align="center" src="assets/img/yt.png" width="60" height="60">
-<img align="center" src="assets/img/ka.png" width="60" height="60">
-<img align="center" src="assets/img/ka1.jpg" width="60" height="60">
